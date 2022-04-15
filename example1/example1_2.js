7bea5e42871c9c29b08a883056caa3f58f915164
SergioFarid <te.puy@hotmail.com>
2022-04-15 14:15:15 -0500

    2.- Elaboración exercises examples 2 y 3

---
+++ example1/example1_2.js
@@ -0,0 +1,9 @@
+console.log("Crear un objeto con propiedades:")
+
+const myObject2 = {
+    name: "Carlo",
+    age: 30
+}
+
+console.log("Ejemplo 2: Objeto con propiedades")
+console.log(myObject2)
---
+++ example1/example1_3.js
@@ -0,0 +1,19 @@
+    
+console.log("Objeto con diferentes propiedades");
+
+const myObject3 = {
+    name: "Tulio",
+    age:30,
+    nicknames: ["Tulipan", "Tulancingo", "Tulish"],
+    address: {
+        zip_code: "526456",
+        street: "Dr. Vertiz 11 Benito Juarez",
+        city: "CDMX"
+    }
+}
+
+console.log("Ejemplo 3: Objeto con diferentes propiedades")
+console.log(myObject3)
+
+console.log(myObject3.name)
+console.log(myObject3["address"])
