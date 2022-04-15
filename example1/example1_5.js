7f2bfbd891435708f3e8d5e9dcc55d94414228f3
SergioFarid <te.puy@hotmail.com>
2022-04-15 14:31:51 -0500

    4. Elaboración exercises example5

---
+++ example1/example1_5.js
@@ -0,0 +1,11 @@
+console.log("Objeto con método que recibe parámetros")
+
+const myPet = {
+    name: "Wopa",
+    sayHelloToMyPet: function (yourPet) {
+        console.log(`${this.name} say's hello to ${yourPet}`)
+    }
+}
+
+console.log("Ejemplo 5: Objeto con método que recibe parámetros")
+myPet.sayHelloToMyPet("Sergio")
