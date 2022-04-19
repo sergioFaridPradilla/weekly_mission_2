0ed789b313501c17eeea6fee64165888ede077f0
SergioFarid <te.puy@hotmail.com>
2022-04-18 21:01:40 -0500

    6. Corrección del example3_6

--- example3/example3_6.js
+++ example3/example3_6.js
@@ -1,2 +1,22 @@
 
-console.log("Esta prueba")
+// Ejemplo 6: Getters para acceder a los atributos del objeto
+
+class Ajolonauta {
+    constructor(name, age, stack){
+     this.name = name
+     this.age = age
+     this.stack = stack
+     this.exercises_completed = 0
+     this.exercises_todo = 99
+    }
+  
+    // Podemos acceder a los atributos de esta clase
+    get getExercisesCompleted() {
+        return this.exercises_completed
+    }
+
+}
+
+console.log("Ejemplo 6: Getters para acceder a los atributos del objeto")
+const woopa = new Ajolonauta("woopa", 1, [])
+console.log( woopa.exercises_completed)
